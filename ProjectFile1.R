f4af42bbbe3ae82c62a1b275a8dc3c4439e49fcb
j-j-ack <91238485+j-j-ack@users.noreply.github.com>
2022-08-22 18:58:32 +0800

    Update ProjectFile1.R

--- ProjectFile1.R
+++ ProjectFile1.R
@@ -1,8 +1,18 @@
 getwd()
 
-setwd("~/")
+setwd("~/capstone/scripts")
 
 
 pacman::p_load(tidyverse)
 rm(list=ls())
 
+READ <- read_csv("~/capstone/data/TCGA_READ_TMM_p400_common_Rboot.csv")
+
+exp <- READ %>% column_to_rownames("gene_id") %>% t()
+
+rm(READ)
+
+# exp$bcr_patient_barcode <- row.names(exp)  
+View(exp)
+
+summarise(exp)
